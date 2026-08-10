8ec485b2519cda7dc6aba1c96e3dc75709361807
Rakibul Islam <rakibshakib74@gmail.com>
2026-08-10 23:38:24 +0600

    feat: integrate Prisma with better-sqlite3 adapter and update user registration logic
    - Added PrismaService to manage database interactions using better-sqlite3 adapter.
    - Updated UserService to fetch users by email and create new users using Prisma.
    - Enhanced AuthService to handle user registration with proper checks for existing users.
    - Updated package.json and yarn.lock to include Prisma dependencies.
    - Adjusted tsconfig.json for module resolution settings.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,15 +5,29 @@ import { RegisterDto } from './dto/register.dto';
 @Injectable()
 export class AuthService {
   constructor(private readonly userService: UserService) {}
-  register(request: RegisterDto) {
+  async register(request: RegisterDto) {
     console.log('Register request:', request);
 
-    const existingUser = this.userService.getUserByEmail(request.email);
+    const existingUser = await this.userService.getUserByEmail(request.email);
     console.log('Existing user:', existingUser);
 
-    if (existingUser) {
-      throw new Error('Email already exists');
+    if (existingUser?.email) {
+      return {
+        message: 'User already exists',
+        status: 403, // forbidden
+      };
     }
+
+    const newUser = await this.userService.createUser(
+      request.email,
+      request.password,
+      request.name,
+    );
+
+    if (newUser?.id) {
+      console.log('New user created:', newUser);
+    }
+
     return {
       message: 'User registered successfully',
     };
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class UserService {
-  getUserByEmail(email: string) {
-    return {
-      name: 'John Doe',
-      email,
-    };
+  constructor(private prisma: PrismaService) {}
+  async getUserByEmail(email: string) {
+    const user = await this.prisma.user.findFirst({
+      where: { email },
+    });
+    return user;
+  }
+
+  // create user
+  async createUser(email: string, password: string, name: string) {
+    const user = await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+    return user;
   }
 }
