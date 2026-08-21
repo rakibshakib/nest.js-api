dc7aec6119e2cd809c244bc46e545f3bc0cd510a
Rakibul Islam <rakibshakib74@gmail.com>
2026-08-22 00:02:20 +0600

    feat: enhance service update functionality with variant handling and pagination in findAll

--- src/services/dto/update-service.dto.ts
+++ src/services/dto/update-service.dto.ts
@@ -1,8 +1,31 @@
-import { PartialType } from '@nestjs/swagger';
-import { IsBoolean } from 'class-validator';
-import { CreateServiceDto } from './create-service.dto';
+import { OmitType, PartialType } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsBoolean,
+  IsInt,
+  IsOptional,
+  IsPositive,
+  ValidateNested,
+} from 'class-validator';
+import { CreateServiceDto, ServiceVariationDto } from './create-service.dto';
 
-export class UpdateServiceDto extends PartialType(CreateServiceDto) {}
+export class UpdateServiceVariantDto extends ServiceVariationDto {
+  @IsOptional()
+  @IsInt()
+  @IsPositive()
+  id?: number;
+}
+
+export class UpdateServiceDto extends PartialType(
+  OmitType(CreateServiceDto, ['variations'] as const),
+) {
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => UpdateServiceVariantDto)
+  variations?: UpdateServiceVariantDto[];
+}
 
 export class UpdateServiceStatusDto {
   @IsBoolean()
--- src/services/services.service.ts
+++ src/services/services.service.ts
@@ -1,7 +1,16 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client';
 import { PrismaService } from 'src/prisma.service';
 import { CreateServiceDto } from './dto/create-service.dto';
-import { UpdateServiceDto } from './dto/update-service.dto';
+import {
+  UpdateServiceDto,
+  UpdateServiceStatusDto,
+  UpdateServiceVariantDto,
+} from './dto/update-service.dto';
 
 @Injectable()
 export class ServicesService {
@@ -49,19 +58,198 @@ export class ServicesService {
     }
   }
 
-  findAll() {
-    return `This action returns all services`;
+  async findAll(limit: number, page: number) {
+    const skip = (page - 1) * limit;
+
+    const [services, total] = await Promise.all([
+      this.prisma.service.findMany({
+        skip,
+        take: limit,
+        orderBy: {
+          createdAt: 'desc',
+        },
+        include: {
+          variations: true,
+        },
+      }),
+
+      this.prisma.service.count(),
+    ]);
+
+    return {
+      data: services,
+      meta: {
+        total,
+        page,
+        limit,
+        totalPages: Math.ceil(total / limit),
+      },
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} service`;
+  async findOne(id: number) {
+    const service = await this.prisma.service.findUnique({
+      where: {
+        id,
+      },
+      omit: {
+        createdById: true,
+      },
+      include: {
+        variations: true,
+        category: true,
+      },
+    });
+
+    if (!service) {
+      throw new NotFoundException('Service not found');
+    }
+
+    return {
+      message: 'Service found successfully',
+      content: service,
+    };
   }
 
-  update(id: number, updateServiceDto: UpdateServiceDto) {
-    return `This action updates a #${id} service`;
+  async update(id: number, updateServiceDto: UpdateServiceDto) {
+    try {
+      const { variations, ...serviceData } = updateServiceDto;
+
+      const service = await this.prisma.$transaction(async (tx) => {
+        await tx.service.update({
+          where: {
+            id,
+          },
+          data: {
+            ...serviceData,
+          },
+        });
+
+        if (variations && variations?.length) {
+          const existingVariant = variations.filter(
+            (variation): variation is UpdateServiceVariantDto =>
+              variation.id !== undefined,
+          );
+
+          const newVariants = variations.filter(
+            (variation) => variation.id === undefined,
+          );
+
+          // Delete removed variations
+          const existingVariantIds = existingVariant
+            .map((variation) => variation.id)
+            .filter((id): id is number => id !== undefined);
+
+          await tx.serviceVariation.deleteMany({
+            where: {
+              serviceId: id,
+              NOT: {
+                id: {
+                  in: existingVariantIds,
+                },
+              },
+            },
+          });
+
+          // Create new variations
+          if (newVariants && newVariants.length) {
+            await tx.serviceVariation.createMany({
+              data: newVariants.map((variation) => ({
+                name: variation.name,
+                price: variation.price,
+                serviceId: id,
+              })),
+            });
+          }
+
+          // update many
+          for (const variation of existingVariant) {
+            await tx.serviceVariation.update({
+              where: {
+                id: variation.id,
+              },
+              data: {
+                name: variation.name,
+                price: variation.price,
+              },
+            });
+          }
+        }
+
+        return tx.service.findUnique({
+          where: {
+            id: id,
+          },
+          include: {
+            variations: true,
+          },
+        });
+      });
+      return {
+        message: 'Service updated successfully',
+        content: service,
+      };
+    } catch (error: unknown) {
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2025'
+      ) {
+        throw new NotFoundException('Service not found');
+      }
+
+      throw new InternalServerErrorException('Failed to update service');
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} service`;
+  async remove(id: number) {
+    try {
+      await this.prisma.service.delete({
+        where: {
+          id,
+        },
+      });
+
+      return {
+        message: 'Category deleted successfully',
+      };
+    } catch (error: unknown) {
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2025'
+      ) {
+        throw new NotFoundException('Category not found');
+      }
+
+      throw new InternalServerErrorException('Failed to delete category');
+    }
+  }
+
+  async updateStatus(id: number, statusDto: UpdateServiceStatusDto) {
+    try {
+      const updated = await this.prisma.service.update({
+        where: {
+          id,
+        },
+        data: {
+          isActive: statusDto.isActive,
+        },
+      });
+
+      return {
+        message: 'Category status updated successfully',
+        content: updated,
+      };
+    } catch (error: unknown) {
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2025'
+      ) {
+        throw new NotFoundException('Category not found');
+      }
+
+      throw new InternalServerErrorException(
+        'Failed to update category status',
+      );
+    }
   }
 }
