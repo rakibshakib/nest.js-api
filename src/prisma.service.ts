13ea2bd9ae0677e3f9926c88da851099e3305ed5
rakibshakib <rakibshakib74@gmail.com>
2026-08-29 00:37:48 +0600

    test deploy

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -5,6 +5,11 @@ import { PrismaClient } from '../generated/prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
+    console.log('DATABASE_URL exists:', !!process.env.DATABASE_URL);
+    console.log(
+      'DATABASE_URL:',
+      process.env.DATABASE_URL?.replace(/:[^:@]+@/, ':****@'),
+    );
     const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
 
     super({ adapter });
