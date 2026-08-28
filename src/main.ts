5b7b668ba81b8624e03d4dcf5ebf21d013700cf9
rakibshakib <rakibshakib74@gmail.com>
2026-08-29 00:03:14 +0600

    update: file

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,10 @@ async function bootstrap() {
   });
 
   app.use(cookieParser());
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe());
 
