1c0e2b084c62ba91736c246047b6abdb8810f56f
rakibshakib <rakibshakib74@gmail.com>
2026-08-29 16:27:58 +0600

    update: server database update

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -5,7 +5,28 @@ import { PrismaClient } from '../generated/prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
-    const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
+    const isProduction = process.env.NODE_ENV === 'production';
+
+    const adapter = new PrismaMariaDb(
+      {
+        host: isProduction ? process.env.DB_HOST! : process.env.LOCAL_DB_HOST!,
+        port: Number(
+          isProduction ? process.env.DB_PORT! : process.env.LOCAL_DB_PORT!,
+        ),
+        user: isProduction ? process.env.DB_USER! : process.env.LOCAL_DB_USER!,
+        password: isProduction
+          ? process.env.DB_PASSWORD!
+          : process.env.LOCAL_DB_PASSWORD!,
+        database: isProduction
+          ? process.env.DB_NAME!
+          : process.env.LOCAL_DB_NAME!,
+        ...(isProduction && {
+          ssl: { rejectUnauthorized: true },
+          connectTimeout: 30000,
+        }),
+      },
+      { useTextProtocol: isProduction },
+    );
 
     super({ adapter });
   }
